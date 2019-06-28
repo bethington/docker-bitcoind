91a63e2ffe5387047e054d0769e6e74cd5e6f62c
Benjamin Ethington <benaminde@gmail.com>
2019-06-27 19:48:23 -0600

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 mv ./data ../data
-docker build --build-arg VERSION=v0.18.0 -t bethington/bitcoind .
-docker build --build-arg VERSION=v0.18.0 -t bethington/bitcoind:v0.18.0 .
+docker build --build-arg VERSION=v0.18.0 --build-arg USER_ID=1000 --build-arg GROUP_ID=1000 -t bethington/bitcoind .
+docker build --build-arg VERSION=v0.18.0 --build-arg USER_ID=1000 --build-arg GROUP_ID=1000 -t bethington/bitcoind:v0.18.0 .
 mv ../data ./data
